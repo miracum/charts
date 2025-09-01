9a268ca12bd963b8eeae1f8bfd2a057771aee494
chgl <chgl@users.noreply.github.com>
2025-09-01 22:59:37 +0200

    fix: lower default resources for postgres sub-chart to nano levels (#666)
    * fix: lower default resources for postgres to nano level
    
    * bumped chart versions
    
    * fix changelog generation

--- .github/renovate-post-upgrade.sh
+++ .github/renovate-post-upgrade.sh
@@ -14,6 +14,16 @@ if [ -z "${newVersion}" ]; then
   exit 1
 fi
 
+# if a PR updates the same dependency across multiple charts,
+# then for each updated chart the name of the dependency is
+# repeated in $depName. the code below removes duplicates.
+# It might be needlessly over-engineered as this also handles
+# cases such as "common common common postgre postgres" which
+# really shouldn't happen.
+# We might want to check out <https://docs.renovatebot.com/configuration-options/#datafiletemplate>
+# eventually to clean this up.
+depName=$(echo "$depName" | tr ' ' '\n' | sort -u | xargs)
+
 echo "Changed dep name is: $depName to $newVersion"
 
 docker run --rm -v "${PWD}:/root/workspace" ghcr.io/chgl/kube-powertools:v2.4.4@sha256:7fb936d1c8f04ea2e74b6de85cb742c093207009798aedb6f61fd6771e8919a5 /root/workspace/.github/renovate-bump-version.sh "${depName}" "${newVersion}"
